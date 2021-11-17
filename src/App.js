87b91438b6a48606225c4bdf70dfb842597af7c8
Sagar Sondur <32958109+sagarsakri@users.noreply.github.com>
2021-11-17 11:42:01 +0530

    change in the title name

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
 
     return (
         <div className="app-header">
-            <p className="heading-text">React Tic Tac Toe - 2020</p>
+            <p className="heading-text">React Tic Tac Toe</p>
             <div className="row jc-center">
                 <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]}/>
                 <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]}/>
